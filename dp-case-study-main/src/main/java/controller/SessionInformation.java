1b94f1af2bca8dc1711d24a905bbde984d92c748
quanqvv <69457581+quanqvv@users.noreply.github.com>
2021-03-05 20:50:43 +0700

    test

--- dp-case-study-main/src/main/java/controller/SessionInformation.java
+++ dp-case-study-main/src/main/java/controller/SessionInformation.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 /**
  * @author
  */
+
+// gây ra vấn đề common coupling của các module khác vào lớp này
+// nên tạo lớp SessionInformation với các thuộc tính như dưới ko dùng static
 public class SessionInformation {
 
     public static User mainUser;
