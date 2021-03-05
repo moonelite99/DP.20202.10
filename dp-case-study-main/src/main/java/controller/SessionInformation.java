07611ccae7d189acd1ba64d5fec05f79d74283ac
quanqvv <69457581+quanqvv@users.noreply.github.com>
2021-03-05 21:01:50 +0700

    test

--- dp-case-study-main/src/main/java/controller/SessionInformation.java
+++ dp-case-study-main/src/main/java/controller/SessionInformation.java
@@ -9,12 +9,11 @@ import java.time.LocalDateTime;
  * @author
  */
 
-// gây ra vấn đề common coupling của các module khác vào lớp này
-// nên tạo lớp SessionInformation với các thuộc tính như dưới ko dùng static
 public class SessionInformation {
 
     public static User mainUser;
     public static Cart cartInstance = new Cart();
     public static LocalDateTime expiredTime;
 // Common Coupling do sử dụng các biến global
+// nên tạo lớp SessionInformation với các thuộc tính như dưới ko dùng static
 }
