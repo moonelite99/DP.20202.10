ccf57cefb8e6a421b452649000f387f3bca94c2a
quanqvv <69457581+quanqvv@users.noreply.github.com>
2021-03-12 19:11:00 +0700

    test gitignore

---
+++ dp-case-study-main/src/main/java/controller/AuthenticationController.java
@@ -0,0 +1,101 @@
+package controller;
+
+import common.exception.ExpiredSessionException;
+import common.exception.FailLoginException;
+import dao.user.UserDAO;
+import entity.user.User;
+import utils.Utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+
+/**
+ * @author
+ */
+public class AuthenticationController extends BaseController {
+
+    public boolean isAnonymousSession() {
+        try {
+            getMainUser();
+            return false;
+        } catch (Exception ex) {
+            return true;
+        }
+    }
+
+
+/*
+/    common coupling vì hàm getMainUser sử dụng chung global data từ class SessionInformation
+
+ */
+    public User getMainUser() throws ExpiredSessionException {
+        if (SessionInformation.mainUser == null || SessionInformation.expiredTime == null || SessionInformation.expiredTime.isBefore(LocalDateTime.now())) {
+            logout();
+            throw new ExpiredSessionException();
+        } else return SessionInformation.mainUser.cloneInformation();
+    }
+
+/*
+/
+    common coupling vì hàm login  sử dụng chung global data từ class SessionInformation
+ */
+    public void login(String email, String password) throws Exception {
+        try {
+            User user = new UserDAO().authenticate(email, md5(password));
+            if (Objects.isNull(user)) throw new FailLoginException();
+            SessionInformation.mainUser = user;
+            SessionInformation.expiredTime = LocalDateTime.now().plusHours(24);
+        } catch (SQLException ex) {
+            throw new FailLoginException();
+        }
+    }
+
+
+/*
+/    common coupling vì hàm logout sử dụng chung global data từ class SessionInformation là mainUser va expiredTime
+ */
+/*
+/  concidental cohesion vì module này làm nhiệm vụ xác thực còn chức năng logout nên để ra 1 module riêng
+ */
+    public void logout() {
+        SessionInformation.mainUser = null;
+        SessionInformation.expiredTime = null;
+    }
+
+
+
+    /**
+     * Return a {@link String String} that represents the cipher text
+     * encrypted by md5 algorithm.
+     *
+     * @param message - plain text as {@link String String}.
+     * @return cipher text as {@link String String}.
+     */
+
+
+// coincidental cohesion vì hàm md5() nên để lại class utils để controller gọi đến khi sử dụng
+    private String md5(String message) {
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(message.getBytes(StandardCharsets.UTF_8));
+            // converting byte array to Hexadecimal String
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            Utils.getLogger(Utils.class.getName());
+            digest = "";
+        }
+        return digest;
+    }
+
+}
---
+++ dp-case-study-main/src/main/java/controller/SessionInformation.java
@@ -0,0 +1,19 @@
+package controller;
+
+import entity.cart.Cart;
+import entity.user.User;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author
+ */
+
+public class SessionInformation {
+    public static User mainUser;
+    public static Cart cartInstance = new Cart();
+    public static LocalDateTime expiredTime;
+// Common Coupling do sử dụng các biến global
+// gây Content Coupling ở các module khác vì khi dùng phải truy cập vào các biến static của lớp này
+// nên tạo lớp SessionInformation với các thuộc tính như dưới ko dùng static
+}
